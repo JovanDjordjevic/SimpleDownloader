f9ff49a6bf9048028c0a9d9afc0b6a3a401b4f64
Jovan Djordjevic <joca.dj@hotmail.com>
2023-11-04 18:44:58 +0100

    Organize programs into sections in GUI

--- downloader.py
+++ downloader.py
@@ -1,63 +1,67 @@
 import tkinter as tk
+from tkinter import ttk
 import subprocess
 
-PROGRAM_NAME_TO_WINGET_ID = {
-    #dev editors
-    "VS Code" : "Microsoft.VisualStudioCode",
-    "Visual Studio Community 2022" : "Microsoft.VisualStudio.2022.Community",
-    "PyCharm Community Edition"  : "JetBrains.PyCharm.Community",
-    "IntelliJ IDEA Community Edition" : "JetBrains.IntelliJIDEA.Community",
-    "Atom" : "GitHub.Atom",
-    "Android Studio" : "Google.AndroidStudio",
-    "Notepad++" : "Notepad++.Notepad++",
-
-    #dev tools
-    "Postman" : "Postman.Postman",
-    "Windows Terminal" : "Microsoft.WindowsTerminal",
-    "Git" : "Git.Git",
-    "Node.js" : "OpenJS.NodeJS",
-    "Python 3.12" : "Python.Python.3.12",
-    "Python 2.7" : "Python.Python.2",
-    "Java 8" : "Oracle.JavaRuntimeEnvironment",
-    "PuTTY" : "PuTTY.PuTTY",
-
-    # communication
-    "Discord" : "Discord.Discord",
-    "Zoom" : "Zoom.Zoom",
-    "Microsoft Teams" : "Microsoft.Teams",
-    "Cisco webex" : "Cisco.WebexTeams",
-    "Telegram Desktop" : "Telegram.TelegramDesktop",
-    "Skype" : "Microsoft.Skype",
-
-    # game launchers
-    "Steam" : "Valve.Steam",
-    "Epic Games Launcher" : "EpicGames.EpicGamesLauncher",
-    "EA app" : "ElectronicArts.EADesktop",
-
-    # media
-    "VLC media player" : "VideoLAN.VLC",
-    "Spotify" : "Spotify.Spotify",
-    "OBS Studio" : "OBSProject.OBSStudio",
-
-    # browsers
-    "Google Chrome" : "Google.Chrome",
-    "Opera GX" : "Opera.OperaGX",
-    "Mozilla Firefox" : "Mozilla.Firefox",
-
-    # other
-    "MSI Afterburner" : "Guru3D.Afterburner",
-    "Rivatuner Statistics Server" : "Guru3D.RTSS",
-    "HWiNFO" : "REALiX.HWiNFO 7.64",
-    "QTTabBar" : "QTTabBar.QTTabBar",
-    "qBittorrent" : "qBittorrent.qBittorrent",
-    "SignalRgb" : "WhirlwindFX.SignalRgb",
-    "Display Driver Uninstaller" : "Wagnardsoft.DisplayDriverUninstaller",
-    "WinRAR" : "RARLab.WinRAR",
-    "7-Zip" : "7zip.7zip",
-    "VMware Workstation Player" : "VMware.WorkstationPlayer",
-    "NZXT CAM" : "NZXT.CAM",
-    "CinebenchR23" : "Maxon.CinebenchR23",
-    "Heaven Benchmark" : "Unigine.HeavenBenchmark",
+# list of programs that are available for installation.
+# Key is "section name" and value is a map where key is "program name" and value is "winget id"
+PROGRAM_LIST = {
+    "Dev editors" : {
+        "VS Code" : "Microsoft.VisualStudioCode",
+        "Visual Studio Community 2022" : "Microsoft.VisualStudio.2022.Community",
+        "PyCharm Community Edition"  : "JetBrains.PyCharm.Community",
+        "IntelliJ IDEA Community Edition" : "JetBrains.IntelliJIDEA.Community",
+        "Atom" : "GitHub.Atom",
+        "Android Studio" : "Google.AndroidStudio",
+        "Notepad++" : "Notepad++.Notepad++",
+    },
+    "Dev tools" : {
+        "Postman" : "Postman.Postman",
+        "Windows Terminal" : "Microsoft.WindowsTerminal",
+        "Git" : "Git.Git",
+        "Node.js" : "OpenJS.NodeJS",
+        "Python 3.12" : "Python.Python.3.12",
+        "Python 2.7" : "Python.Python.2",
+        "Java 8" : "Oracle.JavaRuntimeEnvironment",
+        "PuTTY" : "PuTTY.PuTTY",
+    },
+    "Communication" : {
+        "Discord" : "Discord.Discord",
+        "Zoom" : "Zoom.Zoom",
+        "Microsoft Teams" : "Microsoft.Teams",
+        "Cisco webex" : "Cisco.WebexTeams",
+        "Telegram Desktop" : "Telegram.TelegramDesktop",
+        "Skype" : "Microsoft.Skype",
+    },
+    "Game launchers" : {
+        "Steam" : "Valve.Steam",
+        "Epic Games Launcher" : "EpicGames.EpicGamesLauncher",
+        "EA app" : "ElectronicArts.EADesktop",
+    },
+    "Media" : {
+        "VLC media player" : "VideoLAN.VLC",
+        "Spotify" : "Spotify.Spotify",
+        "OBS Studio" : "OBSProject.OBSStudio",
+    },
+    "Browsers" : {
+        "Google Chrome" : "Google.Chrome",
+        "Opera GX" : "Opera.OperaGX",
+        "Mozilla Firefox" : "Mozilla.Firefox",
+    },
+    "Other" : {
+        "MSI Afterburner" : "Guru3D.Afterburner",
+        "Rivatuner Statistics Server" : "Guru3D.RTSS",
+        "HWiNFO" : "REALiX.HWiNFO 7.64",
+        "QTTabBar" : "QTTabBar.QTTabBar",
+        "qBittorrent" : "qBittorrent.qBittorrent",
+        "SignalRgb" : "WhirlwindFX.SignalRgb",
+        "Display Driver Uninstaller" : "Wagnardsoft.DisplayDriverUninstaller",
+        "WinRAR" : "RARLab.WinRAR",
+        "7-Zip" : "7zip.7zip",
+        "VMware Workstation Player" : "VMware.WorkstationPlayer",
+        "NZXT CAM" : "NZXT.CAM",
+        "CinebenchR23" : "Maxon.CinebenchR23",
+        "Heaven Benchmark" : "Unigine.HeavenBenchmark",
+    },
 }
 
 CHECKBOXES = []
@@ -67,12 +71,12 @@ class ProgramCheckbox:
         self.programName = programName
         self.wingetId = wingetId
 
-        self.checkBoxVar = tk.IntVar()
-        checkbox = tk.Checkbutton(root, text=self.programName, variable=self.checkBoxVar)
-        checkbox.pack()
+        self.checkBoxVar = tk.BooleanVar()
+        checkbox = ttk.Checkbutton(root, text=self.programName, variable=self.checkBoxVar)
+        checkbox.pack(side=tk.LEFT)
 
     def isChecked(self):
-        return self.checkBoxVar.get() == 1
+        return self.checkBoxVar.get() == True
     
     def getProgramName(self):
         return self.programName
@@ -107,14 +111,24 @@ def main():
     root = tk.Tk()
     root.title("Simple downloader")
 
-    label = tk.Label(root, text="Select programs you wish to download:")
-    label.pack()
+    label = ttk.Label(root, text="Select programs you wish to download:")
+    label.pack(side=tk.TOP)
 
-    # Create checkbox for each program
-    for program, wingetId in PROGRAM_NAME_TO_WINGET_ID.items():
-        CHECKBOXES.append(ProgramCheckbox(program, wingetId, root))
+    checkboxSectionsFrame = ttk.Frame(root)
+    checkboxSectionsFrame.pack(padx=10, pady=10)
 
-    downloadButton = tk.Button(root, text="Download selected", command=downloadAllSelected)
+    for sectionName, progToWingetIdMap in PROGRAM_LIST.items():
+        singleSectionFrame = ttk.Frame(checkboxSectionsFrame)
+        singleSectionFrame.pack(side=tk.LEFT, padx=20)
+
+        ttk.Label(singleSectionFrame, text=sectionName).pack(side=tk.TOP)
+
+        for programName, wingetId in progToWingetIdMap.items():
+            checkboxAndIconFrame = ttk.Frame(singleSectionFrame)
+            checkboxAndIconFrame.pack(side=tk.TOP)
+            CHECKBOXES.append(ProgramCheckbox(programName, wingetId, checkboxAndIconFrame))
+
+    downloadButton = ttk.Button(root, text="Download selected", command=downloadAllSelected)
     downloadButton.pack()
 
     root.mainloop()
